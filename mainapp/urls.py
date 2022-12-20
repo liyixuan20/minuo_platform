c439f2fbac2217b15346bbf35a99713f2b762198
liyixuan20 <71712740+liyixuan20@users.noreply.github.com>
2022-12-20 16:27:00 +0800

    backend

---
+++ mainapp/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import *
+from django.conf import settings
+from django.urls import re_path
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('list/', listfunc, name='list'),
+    path('signup/', signupfunc, name='signup'),
+    path('login/', loginfunc, name='login'),
+    path('logout/', logoutfunc, name='logout'),
+    path('setProfile/', setProfilefunc, name='logout'),
+    path('', hwfunc, name='index'),
+    path('profile/', profilefunc, name='profile'),
+    path('upload/', upload_file, name='upload'),
+    path('detail/<int:pk>', detailfunc, name='detail'),
+    path('emails/',EmailReceive, name = 'emails'),
+    re_path(r'^emails/verification/([a-zA-Z0-9]+)/$',VerifyEmail, name = 'Verify'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
